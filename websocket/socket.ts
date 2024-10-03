67acc160007296cd664b9e886c9e80854afa3120
M. Andrés Piana <mateoapiana@gmail.com>
2024-10-03 16:48:01 -0300

    give random color and make groups

--- websocket/socket.ts
+++ websocket/socket.ts
@@ -1,18 +1,31 @@
 import type { Socket } from 'node:dgram'
 import type { DefaultEventsMap } from '../node_modules/socket.io/dist/typed-events'
 
-let users: string[] = []
+let users: [string, string][] = []
+const usersWithoutOpponent: string[] = []
 
 export default async function websocket(
 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
 ) {
-	console.log('a user has connected!')
+	console.log('a user has connected!', { users, usersWithoutOpponent })
 	const { id } = socket
-	users.push(id)
+	usersWithoutOpponent.push(id)
+
+	if (usersWithoutOpponent.length >= 2) {
+		const white = Math.floor(Math.random() * 2)
+		console.log(white)
+
+		users.push([
+			usersWithoutOpponent[white],
+			usersWithoutOpponent[white ? 0 : 1],
+		])
+		usersWithoutOpponent.shift()
+		usersWithoutOpponent.shift()
+	}
 
 	socket.on('disconnect', () => {
-		users = users.filter((i) => i !== id)
+		users = users.filter((i) => !i.includes(id))
 		console.log('an user has disconnected')
 	})
 
@@ -27,7 +40,13 @@ export default async function websocket(
 		}) => {
 			const from = [fromEnemy[0], fromEnemy[1]]
 			const to = [toEnemy[0], toEnemy[1]]
-			socket.to(users.filter((i) => i !== id)[0]).emit('getMove', { from, to })
+			const index = users.findIndex((i) => i.includes(id))
+			console.log(users, index, id)
+
+			if (index !== -1)
+				socket
+					.to(users[index].filter((i) => i !== id)[0])
+					.emit('getMove', { from, to })
 		},
 	)
 }
