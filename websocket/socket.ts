a33afefc3d9732d380ef0bf3532e37d0d183b9cc
M. Andrés Piana <153022165+MateoAPiana@users.noreply.github.com>
2024-11-21 18:38:14 -0300

    fix the back to fix the front routing

--- websocket/socket.ts
+++ websocket/socket.ts
@@ -2,42 +2,50 @@ import type { Socket } from '../node_modules/socket.io/dist/index'
 import type { DefaultEventsMap } from '../node_modules/socket.io/dist/typed-events'
 import type { Server } from '../node_modules/socket.io/dist/index'
 
-let users: [string, string][] = []
-const usersWithoutOpponent: string[] = []
+type user = [string, string]
+let users: [user, user][] = []
+const usersWithoutOpponent: user[] = []
 
 export default async function websocket(
 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
 	io: Server,
 ) {
-	const { id } = socket
-	usersWithoutOpponent.push(id)
-
-	if (usersWithoutOpponent.length >= 2) {
-		const whiteIndex = Math.floor(Math.random() * 2)
-		const blackIndex = whiteIndex === 0 ? 1 : 0
-
-		const whitePlayer = usersWithoutOpponent[whiteIndex]
-		const blackPlayer = usersWithoutOpponent[blackIndex]
-
-		users.push([whitePlayer, blackPlayer])
-
-		io.to(usersWithoutOpponent[0]).emit(
-			'paired',
-			whiteIndex === 1 ? 'white' : 'black',
-		)
-
-		io.to(usersWithoutOpponent[1]).emit(
-			'paired',
-			whiteIndex === 1 ? 'black' : 'white',
-		)
-
-		usersWithoutOpponent.splice(0, 2)
-	}
+	const { id }: { id: string } = socket
 
 	socket.on('disconnect', () => {
-		users = users.filter((i) => !i.includes(id))
-		console.log('an user has disconnected')
+		users = users.filter((i) => !i.some(j => j.includes(id)))
+	})
+
+	socket.on("foundGame", (name: string) => {
+		if (!usersWithoutOpponent.some(i => i[0] === name) &&
+			users.findIndex((i) => i.some(j => j.includes(id))) === -1 &&
+			!usersWithoutOpponent.some(i => i[1] === id)) {
+			usersWithoutOpponent.push([name, id])
+			if (usersWithoutOpponent.length >= 2) {
+				const whiteIndex = Math.floor(Math.random() * 2)
+				const blackIndex = whiteIndex === 0 ? 1 : 0
+
+				const whitePlayer = usersWithoutOpponent[whiteIndex]
+				const blackPlayer = usersWithoutOpponent[blackIndex]
+
+				users.push([whitePlayer, blackPlayer])
+
+				io.to(usersWithoutOpponent[0]).emit(
+					'paired',
+					whiteIndex === 1 ? 'white' : 'black',
+				)
+
+				io.to(usersWithoutOpponent[1]).emit(
+					'paired',
+					whiteIndex === 1 ? 'black' : 'white',
+				)
+
+				usersWithoutOpponent.splice(0, 2)
+				console.log({ usersWithoutOpponent, users })
+			}
+			console.log({ name, id })
+		}
 	})
 
 	socket.on(
@@ -51,10 +59,11 @@ export default async function websocket(
 		}) => {
 			const from = [7 - fromEnemy[0], 7 - fromEnemy[1]]
 			const to = [7 - toEnemy[0], 7 - toEnemy[1]]
-			const index = users.findIndex((i) => i.includes(id))
+			const index = users.findIndex((i) => i.some(j => j.includes(id)))
+			console.log({ fromEnemy, toEnemy, index, users: users[0] })
 			if (index !== -1)
 				socket
-					.to(users[index].filter((i) => i !== id)[0])
+					.to(users[index].filter((i) => i[1] !== id)[0])
 					.emit('getMove', { from, to })
 		},
 	)
@@ -62,7 +71,7 @@ export default async function websocket(
 	socket.on(
 		"end",
 		async (color: string) => {
-			const usersToEmit = users.filter(i => i.includes(id)).flat()
+			const usersToEmit = users.filter(i => !i.some(j => j.includes(id))).flat()
 			console.log(usersToEmit)
 			socket.to(usersToEmit[0]).emit("winner", color)
 			socket.to(usersToEmit[1]).emit("winner", color)
